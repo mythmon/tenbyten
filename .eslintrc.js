b205d54a373ff2a505dbba99730c024c47321809
Mike Cooper <mythmon@gmail.com>
2017-03-23 10:23:51 -0700

    Install flow with no @flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,11 @@ module.exports = {
     'standard-react',
     'plugin:import/errors',
     'plugin:import/warnings',
+    'plugin:flowtype/recommended'
   ],
   plugins: [
     'babel',
+    'flowtype',
   ],
   parser: 'babel-eslint',
   env: {
@@ -14,8 +16,16 @@ module.exports = {
   },
   settings: {
     'import/resolver': 'webpack',
+    'flowtype': {
+      'onlyFilesWithFlowAnnotation': true,
+    },
   },
   rules: {
     'comma-dangle': ['warn', 'always-multiline'],
+    'no-undef': ['error'],
+    'no-duplicate-imports': ['off'], // does not support flow type imports
+    'import/no-duplicates': ['error'], // support flow types imports
+    'flowtype/require-valid-file-annotation': ['error', 'always', {annotationStyle: 'line'}],
+    'react/no-unused-prop-types': ['off'],
   },
 };
